0c7923c78f98ec2c4197790d9bd34fc3fcdef30b
FOKA <onlyfoka@yahoo.com>
2025-05-31 11:33:31 -0300

    home

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,13 +1,22 @@
-var express = require ('express')
-var routes = require ('./routes')
- var app = express ();
+var express = require('express')
+ , load = require('express-load')
+  , app = express();
+
+var routes = require('./routes/home')
+
+load('models')
+   .then('controllers')
+   .then('routes')
+   .into(app);
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app get('/', routes.index);
-app.get('/usuarios', routes.unsubscribe.index);
+
+app.get("/i","sobre.html")
+//app.get('/usuarios', routes.unsubscribe.index);
 
 app.listen(3000, function(){
   console.log("Ntalk no ar.");
